2f87aa40c0556ef3310fc7c1a9a058fe6b222938
darius-xelphur <darius.soh12@gmail.com>
2026-09-12 12:26:33 +0800

    test: redeployment

--- test_connection.py
+++ test_connection.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-from google.oauth2 import service_account
+from google.oauth2 import service_account # type: ignore
 
 # Load credentials
 credentials = service_account.Credentials.from_service_account_file(
